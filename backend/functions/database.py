b23a4424ca7a4db191c56df51addd2d7d574c84b
Latin Warrior <ninjaguy@zoho.com>
2023-09-10 16:49:59 -0500

    Rachel can have a conversation about food now.

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -7,7 +7,7 @@ def get_recent_messages():
     file_name = 'stored_data.json'
     learn_instruction = {
         "role": "system",
-        "content": "You are interviewing the user for a job as a retail assistant. Ask short questions that are relevant to the junior position. Your name is Rachel. The user is called Luis. Keep your answers under 30 words."
+        "content": "You are food connoisseur. Ask short questions that are relevant to finding food recipes. Your name is Rachel the great. The user is called Ms. Customer. Keep your answers under 30 words."
     }
 
     # Add a random element
--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -2,20 +2,78 @@ import React from 'react';
 import { useState } from 'react';
 
 import Title from './Title';
+import RecordMessage from './RecordMessage';
+import axios from 'axios';
 
 const Controller = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [messages, setMessages] = useState<any[]>([]);
 
-    const createBlobUrl = (data: any) => {};
+    const createBlobUrl = (data: any) => {
+        const blob = new Blob([data], { type: 'audio/mpeg' });
+        return window.URL.createObjectURL(blob);
+    };
 
-    const handleStop = async () => {};
+    const handleStop = async (blobUrl: string) => {
+        setIsLoading(true);
+
+        // Append recorded message to messages.
+        const myMessage = { sender: 'me', blobUrl };
+        const messageArr = [...messages, myMessage];
+
+        // Convert blog URL to blob object.
+        fetch(blobUrl)
+            .then((res) => res.blob())
+            .then(async (blob) => {
+                // Construct audio to send file.
+                const formData = new FormData();
+
+                formData.append('file', blob, 'myFile.wav');
+
+                // Send form data to API endpoint.
+                await axios
+                    .post('http://localhost:8000/post-audio', formData, {
+                        headers: { 'Content-Type': 'audio/mpeg' },
+                        responseType: 'arraybuffer',
+                    })
+                    .then((res: any) => {
+                        const blob = res.data;
+                        const audio = new Audio();
+                        audio.src = createBlobUrl(blob);
+
+                        // Append to audio.
+                        const rachelMessage = {
+                            sender: 'rachel',
+                            blobUrl: audio.src,
+                        };
+
+                        messageArr.push(rachelMessage);
+
+                        setMessages(messageArr);
+
+                        setIsLoading(false);
+                        // Play Audion
+                        audio.play();
+                    })
+                    .catch((err) => {
+                        console.error(err.message);
+                        setIsLoading(false);
+                    });
+            });
+
+        setIsLoading(false);
+    };
 
     return (
         <div className='h-screen overflow-y-hidden'>
             <Title setMessages={setMessages} />
             <div className='flex flex-col justify-between h-full overflow-y-scroll pb-96'>
-                Placeholder
+                {/* Recorder */}
+                <div className='fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500'>
+                    <div className='flex justify-center items-center w-full'>
+                        <RecordMessage handleStop={handleStop} />
+                    </div>
+                </div>
             </div>
         </div>
     );
